3b1ece5030fb6c5e64e2cae3435ac2b312425859
Nguyễn Trung Hậu <trunghauad02@gmail.com>
2024-05-10 03:17:07 +0700

    Cap nhat lesson

--- src/main/java/com/englishforadmin/controller/Lesson_editB1Controller.java
+++ src/main/java/com/englishforadmin/controller/Lesson_editB1Controller.java
@@ -1,11 +1,12 @@
 package com.englishforadmin.controller;
 
 import com.englishforadmin.StateManager;
+import com.englishforadmin.daoimpl.LessonDAOimpl;
+import com.englishforadmin.feature.MessageBox;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import com.englishforadmin.MainApplication;
 import io.github.palexdev.materialfx.controls.MFXButton;
-import io.github.palexdev.materialfx.controls.MFXRadioButton;
 
 import javafx.scene.Node;
 import javafx.scene.Scene;
@@ -62,6 +63,9 @@ public class Lesson_editB1Controller {
 
     @FXML
     private TextArea txtareaDescription;
+    @FXML
+    private TextField txtSerial;
+    LessonDAOimpl lessonDAO;
 
     @FXML
     void CancelLesson_editB1(ActionEvent event ) throws IOException
@@ -76,7 +80,32 @@ public class Lesson_editB1Controller {
     @FXML
     void Lesson_editB2Screen(ActionEvent event ) throws IOException
     {
-        // submit lesson next B1
+        if(isMissData()){
+            MessageBox.show("Lỗi","Hãy điền đầy đủ thông tin trước khi tiếp tục", Alert.AlertType.ERROR);
+            return;
+        }
+        if(isUpdateData()){
+            if(StateManager.getCurrentLesson().getSerial() != Integer.parseInt(txtSerial.getText().trim())
+                    && isDuplicateSerial(Integer.parseInt(txtSerial.getText().trim()))){
+                MessageBox.show("Lỗi","Số thứ tự bài học này đã tồn tại", Alert.AlertType.ERROR);
+                return;
+            }
+            Lesson lesson = new Lesson();
+            lesson.setIdLesson(StateManager.getCurrentLesson().getIdLesson());
+            if(rdbOpenLesson.isSelected())
+                lesson.setStatus(Lesson.LessonStatus.unlock);
+            else
+                lesson.setStatus(Lesson.LessonStatus.lock);
+            lesson.setSerial(Integer.parseInt(txtSerial.getText()));
+            lesson.setName(txtNameLesson.getText());
+            lesson.setDescription(txtareaDescription.getText());
+            if(!lessonDAO.update(lesson)){
+                MessageBox.show("Lỗi","Cập nhật bài học thất bại", Alert.AlertType.ERROR);
+                return;
+            }
+            StateManager.setCurrentLesson(lesson);
+            MessageBox.show("Thông báo","Cập nhật bài học thành công", Alert.AlertType.CONFIRMATION);
+        }
         try {
             MainApplication.loadForm("/lesson", "Lesson_editB2.fxml");
         } catch (IOException e) {
@@ -97,12 +126,48 @@ public class Lesson_editB1Controller {
 
     @FXML
     public void initialize() {
-        // Load dữ liệu từ đối tượng Lesson đã chọn
+        lessonDAO = new LessonDAOimpl();
         Lesson lesson = StateManager.getCurrentLesson();
+        loadToggleGroup();
+        txtSerial.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.matches("\\d*")) {
+                txtSerial.setText(newValue.replaceAll("[^\\d]", ""));
+            }
+        });
         if (lesson != null) {
             txtNameLesson.setText(lesson.getName());
             txtareaDescription.setText(lesson.getDescription());
+            if (lesson.getStatus() == Lesson.LessonStatus.unlock) {
+                rdbOpenLesson.setSelected(true);
+            } else {
+                rdbLockLesson.setSelected(true);
+            }
+            txtSerial.setText(String.valueOf(lesson.getSerial()));
         }
     }
+    private void loadToggleGroup(){
+        ToggleGroup toggleGroup = new ToggleGroup();
+        rdbLockLesson.setToggleGroup(toggleGroup);
+        rdbOpenLesson.setToggleGroup(toggleGroup);
+    }
+    private boolean isMissData(){
+        return txtNameLesson.getText().isEmpty() || txtareaDescription.getText().isEmpty() || txtSerial.getText().isEmpty();
+    }
 
+    private boolean isDuplicateSerial(int serial){
+        return lessonDAO.checkDuplicateSerial(serial);
+    }
+
+    private boolean isUpdateData(){
+        Lesson curLesson = StateManager.getCurrentLesson();
+        boolean isChangeStatus = true;
+        if(curLesson.getStatus() == Lesson.LessonStatus.unlock && rdbOpenLesson.isSelected())
+            isChangeStatus = false;
+        else if(curLesson.getStatus() == Lesson.LessonStatus.lock && rdbLockLesson.isSelected())
+            isChangeStatus = false;
+        return !curLesson.getName().equals(txtNameLesson.getText())
+                || !curLesson.getDescription().equals(txtareaDescription.getText())
+                || curLesson.getSerial() != Integer.parseInt(txtSerial.getText().trim())
+                || isChangeStatus;
+    }
 }
